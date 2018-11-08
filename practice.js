5549834afeda63b278a4aaafdb9f377e8aa90967
Michelle Hoelzer <hoelzermm@gmail.com>
2018-11-08 10:50:09 -0500

    finished bonus 2?

--- practice.js
+++ practice.js
@@ -18,7 +18,7 @@ class ControllableTile extends Tile {
     handleEvent(evt) {
         if (evt.type === "keypress") {
             switch (evt.key) {
-                case "ArrowLeft": 
+                case "ArrowLeft":
                     this.x--;
                     break;
                 case "ArrowRight":
@@ -105,3 +105,97 @@ PlayerTile.prototype = Object.create(ControllableTile.prototype);
  * would the syntax be? Write a class for FloorTile, giving it whatever new
  * properties or methods you think make sense.
  */
+// const FloorTile = new SolidTile(x, y, type);
+// ORRR
+class FloorTile extends SolidTile {
+    constructor(x, y, type) {
+        super(x, y, type);
+    };
+};
+// ORRR MAKE just extends/new Tile?
+
+
+
+/*
+// AS PROTOTYPE
+function Tile(x, y, type) {
+    this.x = x;
+    this.y = y;
+    // type helps determine the postitioning (?Stateic vs absoleute) and background color of tile once drawn
+    this.type = type;
+  };
+// these tiles are drawn, no interaction
+const floor = new Tile(0, 0, "floor");
+const storage = new Title(2, 5, "storage");
+// Tile.call(...) = call Tile constructor method, but attach results to the ControllableTile instead of Tile
+function ControllableTile(x, y, type) {
+Tile.call(x, y, type);
+};
+// inherit functionality from Tile 
+ControllableTile.prototype = Object.create(Tile.prototype);
+// controlablleTitle controllable
+ControllableTile.prototype.handleEvent = function(evt) {
+if (evt.type === "keypress") {
+    switch (evt.key) {
+    case "ArrowLeft": 
+        this.x--;
+        break;
+    case "ArrowRight":
+        this.x++;
+        break;
+    case "ArrowUp":
+        this.y--;
+        break;
+    case "ArrowDown":
+        this.y++;
+        break;
+    }
+}
+};
+// player associate with dom
+const playerElement = document.getElementById("player");
+const playerTile = new ControllableTile(0, 0, "player");
+playerElement.addEventListener("keypress", playerTile, false);
+
+
+// AS CLASS
+// constructor of a class is explicit, and that the whole thing is inside this new class construct, delimited by a block (opening and closing curly braces)
+class Tile = {
+constructor(x, y, type) {
+    this.x = x;
+    this.y = y;
+    this.type = type;
+};
+};
+const floor = new Tile(0, 0, "floor");
+const storage = new Tile(2, 5, "storage");
+// extends ~= ControllableTile.prototype = Object.create(Tile.prototype);
+class ControllableTile extends Tile {
+constructor(x, y, type) {
+    // super = Tile.call()
+    super(x, y, type);
+};
+// methods are handled inside constructor 
+handleEvent(event) {
+    if(event.type = "keypress") {
+    switch (event.key) {
+        case "ArrowLeft": 
+        this.x--;
+        break;
+        case "ArrowRight":
+        this.x++;
+        break;
+        case "ArrowUp":
+        this.y--;
+        break;
+        case "ArrowDown":
+        this.y++;
+        break;
+    };
+    };
+};
+};
+const playerElement = document.getElementById("player");
+const playerTile = new ControllableTile(0, 0, "player");
+playerElement.addEventListener("keypress", playerTile, false);
+*/
